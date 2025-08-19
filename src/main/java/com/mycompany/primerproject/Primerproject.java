0798717033edaa37edd1ac844680e28b4e94056c
Pedro Angulo <anpe88@hotmail.com>
2025-08-18 22:37:10 -0500

    PRIMERA CARGA DE ARCHIVOS

---
+++ src/main/java/com/mycompany/primerproject/Primerproject.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.primerproject;
+
+public class Primerproject {
+
+    public static void main(String[] args) {
+        
+        int x=10;
+        
+        System.out.println("X = "+ x);
+    }
+}
