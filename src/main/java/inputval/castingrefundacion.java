d26d3f0d5c8271040ce5be3045f98f0ed14e351f
Pedro Angulo <anpe88@hotmail.com>
2025-08-21 21:25:32 -0500

    septima carga con condicional final para constantes, byte y utilidad de libreria math

---
+++ src/main/java/inputval/castingrefundacion.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inputval;
+
+/**
+ *
+ * @author Usuario
+ */
+public class castingrefundacion {
+    
+     public static void main (String[] args){
+        
+        byte num1 = 2;
+        byte num2 = 5;
+        
+        byte resultado = (byte) (num1 + num2);
+        
+        double num3 = 9;
+        
+        double raizc = Math.sqrt(num3);
+        
+        System.out.println("valor: "+ resultado);
+        System.out.println("valor: "+ raizc);
+        
+    }
+    
+}
